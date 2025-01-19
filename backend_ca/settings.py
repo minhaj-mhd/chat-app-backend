ac437408f95940a69491a68953d54d93a9f7baba
minhaj-mhd <minhajmuhamd@gmail.com>
2025-01-19 13:10:41 +0530

    allowed render hosts

--- backend_ca/settings.py
+++ backend_ca/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chat-app-backend-gmjh.onrender.com']
+ALLOWED_HOSTS = ['chat-app-backend-gmjh.onrender.com',]
 
 
 # Application definition
